61c84ddd28d8686d8159a76a19b390ecaf4e6119
Alex <114315489+kashirin-olexsandr@users.noreply.github.com>
2025-01-15 15:59:50 +0200

    w

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    base: '/goit-js-hw-10/',
     root: 'src',
     build: {
       sourcemap: true,
